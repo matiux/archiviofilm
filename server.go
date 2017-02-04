e71a206c7bc1017ee090f270f3c87b6db85bdb28
matiux <m.galacci@gmail.com>
2017-02-04 15:54:49 +0100

    WIP - server

--- server.go
+++ server.go
@@ -1,11 +1,12 @@
 package main
 
 import (
-	"encoding/json"
-	// "fmt"
 	"bitbucket.org/matiux/archiviofilm/service"
+	"encoding/json"
+	"fmt"
 	"github.com/gorilla/mux"
-	"io"
+	"html"
+	// "io"
 	"log"
 	"net/http"
 )
@@ -14,14 +15,35 @@ func FilmListEndPoint(w http.ResponseWriter, req *http.Request) {
 
 	films := service.NewListFilm().Execute()
 
-	// res2B, _ := json.Marshal(films)
+	w.Header().Set("Content-Type", "application/json")
+
+	if err := json.NewEncoder(w).Encode(films); err != nil {
+		panic(err)
+	}
+
+	//serialized, _ := json.Marshal(films)
 
 	// io.WriteString(w, string(res2B))
+
+	//io.WriteString(w, string(serialized))
+
+	//fmt.Fprintf(w, "Hello, %q", html.EscapeString(req.URL.Path))
+
+	fmt.Printf("Endpoint Hit: %v [FilmListEndPoint()]\n", html.EscapeString(req.URL.Path))
+}
+
+func FilmUpdateEndPoint(w http.ResponseWriter, req *http.Request) {
+
+	params := mux.Vars(req)
+
+	fmt.Println(params)
 }
 
 func main() {
 
-	router := mux.NewRouter()
+	router := mux.NewRouter().StrictSlash(true)
+	router.Host("localhost:8080")
 	router.HandleFunc("/films", FilmListEndPoint).Methods("GET")
-	log.Fatal(http.ListenAndServe(":8000", router))
+	router.HandleFunc("/films/{film}", FilmUpdateEndPoint).Methods("PATCH")
+	log.Fatal(http.ListenAndServe(":8080", router))
 }
